61d7c8576a3d93f0f8fe125ae07247c8dbdc2b91
Yasmim <ymdsc.coimbra85@gmail.com>
2025-02-27 18:26:56 -0300

    adding color  change back to blue

--- exercicio-1/index.js
+++ exercicio-1/index.js
@@ -7,5 +7,11 @@ let blockToChange = document.getElementsByClassName('upper-block')[0]
 
 colorSwitchBtn
 .addEventListener("click", function trocarCor() {
-    blockToChange.classList.add('changed-block')
+    if (blockToChange.classList.contains('upper-block')) {
+        blockToChange.classList.remove('upper-block')
+        blockToChange.classList.add('changed-block')
+    } else {
+        blockToChange.classList.remove('changed-block')
+        blockToChange.classList.add('upper-block')
+    }
 })
